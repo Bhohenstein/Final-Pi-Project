eb1f82232fa0c7df1f8143ce2c5b60dbae7bb4bb
Brenden Hohenstein <75498943+Bhohenstein@users.noreply.github.com>
2021-02-04 14:16:08 -0500

    Update Final_Pi_Program.py

--- Final_Pi_Program.py
+++ Final_Pi_Program.py
@@ -5,9 +5,10 @@
 
 from tkinter import *
 import pygame
+from time import sleep
 
 pygame.init()
-Piano = [pygame.mixer.Sound("Piano.ff.A0.aiff")]
+Piano = [pygame.mixer.Sound("Piano.ff.A0.aiff"),pygame.mixer.Sound("Piano.ff.C2.aiff"),pygame.mixer.Sound("Piano.mf.Eb2.aiff")]
 Keyboard = []
 
 sounds = Piano
@@ -44,68 +45,69 @@ class MainGUI(Frame):
         #########################################################################
         
         #The Piano Keys################################################################################################
-        #the ipadx and ipady control how big the keys are, these will probably need to be tested and changed for the PI's screen.
         #White Keys:
         self.key1 = Button(self.parent, bg="white",command = lambda: sounds[0].play())
-        self.key1.grid(row=1,column=1,ipadx=30,ipady=100)
+        self.key1.grid(row=1,column=1,ipadx=15,ipady=100)
 
-        self.key3 = Button(self.parent, bg="white")
-        self.key3.grid(row=1,column=2,ipadx=30,ipady=100)
+        self.key3 = Button(self.parent, bg="white",command = lambda: sounds[1].play())
+        self.key3.grid(row=1,column=2,ipadx=15,ipady=100)
 
-        self.key4 = Button(self.parent, bg="white")
-        self.key4.grid(row=1,column=3,ipadx=30,ipady=100)
+        self.key4 = Button(self.parent, bg="white",command = lambda: sounds[1].play())
+        self.key4.grid(row=1,column=3,ipadx=15,ipady=100)
 
-        self.key6 = Button(self.parent, bg="white")
-        self.key6.grid(row=1,column=4,ipadx=30,ipady=100)
+        self.key6 = Button(self.parent, bg="white",command = lambda: sounds[1].play())
+        self.key6.grid(row=1,column=4,ipadx=15,ipady=100)
 
         self.key7 = Button(self.parent, bg="white")
-        self.key7.grid(row=1,column=5,ipadx=30,ipady=100)
+        self.key7.grid(row=1,column=5,ipadx=15,ipady=100)
 
         self.key8 = Button(self.parent, bg="white")
-        self.key8.grid(row=1,column=6,ipadx=30,ipady=100)
+        self.key8.grid(row=1,column=6,ipadx=15,ipady=100)
 
         self.key9 = Button(self.parent, bg="white")
-        self.key9.grid(row=1,column=7,ipadx=30,ipady=100)
+        self.key9.grid(row=1,column=7,ipadx=15,ipady=100)
 
         self.key11 = Button(self.parent, bg="white")
-        self.key11.grid(row=1,column=8,ipadx=30,ipady=100)
+        self.key11.grid(row=1,column=8,ipadx=15,ipady=100)
 
         self.key12 = Button(self.parent, bg="white",)
-        self.key12.grid(row=1,column=9,ipadx=30,ipady=100)
+        self.key12.grid(row=1,column=9,ipadx=15,ipady=100)
 
         self.key14 = Button(self.parent, bg="white")
-        self.key14.grid(row=1,column=10,ipadx=30,ipady=100)
+        self.key14.grid(row=1,column=10,ipadx=15,ipady=100)
 
         self.key15 = Button(self.parent, bg="white",)
-        self.key15.grid(row=1,column=11,ipadx=30,ipady=100)
+        self.key15.grid(row=1,column=11,ipadx=15,ipady=100)
 
         self.key17 = Button(self.parent, bg="white")
-        self.key17.grid(row=1,column=12,ipadx=30,ipady=100)
+        self.key17.grid(row=1,column=12,ipadx=15,ipady=100)
 
         #Black Keys;
         self.key2 = Button(self.parent, bg="black")
-        self.key2.grid(row=1,column=1,columnspan=2,ipadx=30,ipady=55,sticky= N)
+        self.key2.grid(row=1,column=1,columnspan=2,ipadx=15,ipady=55,sticky= N)
     
         self.key5 = Button(self.parent, bg="black")
-        self.key5.grid(row=1,column=3,columnspan=2,ipadx=30,ipady=55,sticky= N)
+        self.key5.grid(row=1,column=3,columnspan=2,ipadx=15,ipady=55,sticky= N)
         
         self.key7 = Button(self.parent, bg="black")
-        self.key7.grid(row=1,column=5,columnspan=2,ipadx=30,ipady=55,sticky= N)
+        self.key7.grid(row=1,column=5,columnspan=2,ipadx=15,ipady=55,sticky= N)
     
         self.key10 = Button(self.parent, bg="black")
-        self.key10.grid(row=1,column=7,columnspan=2,ipadx=30,ipady=55,sticky= N)
+        self.key10.grid(row=1,column=7,columnspan=2,ipadx=15,ipady=55,sticky= N)
 
         self.key13 = Button(self.parent, bg="black")
-        self.key13.grid(row=1,column=9,columnspan=2,ipadx=30,ipady=55,sticky= N)
+        self.key13.grid(row=1,column=9,columnspan=2,ipadx=15,ipady=55,sticky= N)
 
         self.key16 = Button(self.parent, bg="black")
-        self.key16.grid(row=1,column=11,columnspan=2,ipadx=30,ipady=55,sticky= N)
+        self.key16.grid(row=1,column=11,columnspan=2,ipadx=15,ipady=55,sticky= N)
         #######################################################################################
 
 
         #The default songs#####################################################################
         def stars(): #Plays "twinkle twinkle little star"
             self.key1.invoke()
+            sleep(0.1)
+            self.key3.invoke()
         def lambs(): #Plays "mary had a little lamb"
             self.key1.invoke()
 
